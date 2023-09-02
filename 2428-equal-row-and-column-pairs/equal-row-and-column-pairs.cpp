a7a32c0a2281992e7eb5a8e29e8ee1f8b76d14c6
Mohamed AMOUSSAOUI <58079564+mamoussa405@users.noreply.github.com>
2023-09-02 17:07:45 +0100

    Time: 326 ms (15.22%) | Memory: 26.1 MB (76.56%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
         int n = grid.size();
-        vector<vector<int>> cols(n, vector<int>(n));
+        // vector<vector<int>> cols(n, vector<int>(n));
         vector<vector<int>> rows(n, vector<int>(n)); 
         int ans = 0;
 
-        for (int i = 0; i < n; ++i)
-            for (int j = 0; j < n; ++j)
-                cols[i][j] = grid[i][j];
+        // for (int i = 0; i < n; ++i)
+        //     for (int j = 0; j < n; ++j)
+        //         cols[i][j] = grid[i][j];
         for (int i = 0; i < n; ++i)
             for (int j = 0; j < n; ++j)
                 rows[i][j] = grid[j][i];
@@ -19,7 +19,7 @@ public:
                 int j = 0;
                 while (j < n)
                 {
-                    if (cols[i][j] != rows[k][j])
+                    if (grid[i][j] != rows[k][j])
                         break;
                     ++j;
                 }
