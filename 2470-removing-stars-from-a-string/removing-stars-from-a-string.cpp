51c2d254773c858c2c28e4d1316b6be248aa1976
Mohamed AMOUSSAOUI <58079564+mamoussa405@users.noreply.github.com>
2023-09-02 17:20:40 +0100

    Time: 88 ms (55.57%) | Memory: 26 MB (58.46%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string ans;
+
+        for (char& c : s) 
+        {
+            if (c == '*' && !ans.empty()) ans.pop_back();
+            else ans += c;
+        }
+        return ans;
+    }
+};
