5b3c4a86d7d86aa5532d83427796fc4b82751d18
Mohamed AMOUSSAOUI <58079564+mamoussa405@users.noreply.github.com>
2023-09-12 22:20:27 +0100

    Time: 14 ms (6.82%) | Memory: 20.6 MB (11.30%) - LeetSync

--- 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.cpp
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.cpp
@@ -3,8 +3,10 @@ public:
     int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
         int ans = 0;
 
-        for (int& n : hours) 
-            ans += (n >= target);
+        // for (int& n : hours) 
+        //     ans += (n >= target);
+        for (int i = 0; i < hours.size(); ++i)
+            ans += (hours[i] >= target);
         return ans;
     }
 };
