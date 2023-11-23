2e3c58dd7b7ec8fb733e85899b723bd5241ff91c
Mohamed AMOUSSAOUI <58079564+mamoussa405@users.noreply.github.com>
2023-11-23 22:07:53 +0100

    Time: 81 ms (49.79%) | Memory: 16.8 MB (14.81%) - LeetSync

--- 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.py
+++ 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.py
@@ -1,41 +1,15 @@
 class Solution:
     def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
-        origX: int = 0
-        origY: int = 1
-        m: int = len(mat)
-        n: int = len(mat[0])
+        map: dict[int, int] = defaultdict(list)
 
-        while origX < n:
-            x: int = origX
-            y: int = 0
-            tmpList: list[int] = []
-            while x < n and y < m:
-                tmpList.append(mat[y][x])
-                x += 1
-                y += 1
-            tmpList.sort()
-            x = origX
-            y = 0
-            for num in tmpList:
-                mat[y][x] = num
-                y += 1
-                x += 1
-            origX += 1
-
-        while origY < m:
-            x: int = 0
-            y: int = origY
-            tmpList: list[int] = []
-            while x < n and y < m:
-                tmpList.append(mat[y][x])
-                x += 1
-                y += 1
-            tmpList.sort()
-            x = 0
-            y = origY
-            for num in tmpList:
-                mat[y][x] = num
-                y += 1
-                x += 1
-            origY += 1
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                map[i - j].append(mat[i][j])
+        
+        for v in map.values():
+            v.sort()
+        
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                mat[i][j] = map[i-j].pop(0)
         return mat
