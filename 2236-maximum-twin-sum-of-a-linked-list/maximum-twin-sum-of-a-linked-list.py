92a32f6226848831daec4a3a28807503bb718ae1
Mohamed AMOUSSAOUI <58079564+mamoussa405@users.noreply.github.com>
2023-11-26 23:09:42 +0100

    Time: 686 ms (37.89%) | Memory: 47.6 MB (58.82%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
@@ -10,21 +10,21 @@ class Solution:
         while fast and fast.next and fast.next.next:
             fast = fast.next.next
             slow = slow.next
-
-        def reverse_list(currNode: Optional[ListNode], parent: Optional[ListNode]) -> Optional[ListNode]:
-            if currNode.next == None:
-                currNode.next = parent
-                return currNode
-            newHead: ListNode = reverse_list(currNode.next, currNode)
-            currNode.next = parent
-            return newHead
         
-        leftHead: Optional[ListNode] = reverse_list(slow.next, None)
+        curr: LisNode = slow.next
+        prev: ListNode = None
+
+        while curr:
+            nextNode = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextNode
+        
         ans: int = 0
-        while head and leftHead:
-            ans = max(ans, head.val + leftHead.val)
+        while head and prev:
+            ans = max(ans, head.val + prev.val)
             head = head.next
-            leftHead = leftHead.next
+            prev = prev.next
         return ans
 
         
