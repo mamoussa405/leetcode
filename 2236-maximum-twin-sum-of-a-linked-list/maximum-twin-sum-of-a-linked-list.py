36eabc75769ef3c5dc06a7aaa670c5632c13a2e7
Mohamed AMOUSSAOUI <58079564+mamoussa405@users.noreply.github.com>
2023-11-26 22:59:50 +0100

    Time: 784 ms (7.79%) | Memory: 88.8 MB (5.05%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
@@ -5,18 +5,26 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        arr: list[int] = []
-        start: int = 0
-        end: int = 0
-        ans: int = 0
+        slow, fast = head, head
 
-        while head:
-            arr.append(head.val)
+        while fast and fast.next and fast.next.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        def reverse_list(currNode: Optional[ListNode], parent: Optional[ListNode]) -> Optional[ListNode]:
+            if currNode.next == None:
+                currNode.next = parent
+                return currNode
+            newHead: ListNode = reverse_list(currNode.next, currNode)
+            currNode.next = parent
+            return newHead
+        
+        leftHead: Optional[ListNode] = reverse_list(slow.next, None)
+        ans: int = 0
+        while head and leftHead:
+            ans = max(ans, head.val + leftHead.val)
             head = head.next
-        end = len(arr) - 1
-        while start < end:
-            ans = max(ans, (arr[start] + arr[end]))
-            start += 1
-            end -= 1
+            leftHead = leftHead.next
         return ans
+
         
