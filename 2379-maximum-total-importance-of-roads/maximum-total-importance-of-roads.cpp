052c1cb6f6784cccdc37c581409afd71f076ed96
Mohamed AMOUSSAOUI <58079564+mamoussa405@users.noreply.github.com>
2024-06-28 12:07:42 +0100

    Time: 396 ms (28.11%) | Memory: 135.9 MB (45.52%) - LeetSync

---
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long maximumImportance(int n, vector<vector<int>>& roads) {
+        map<int, long long> f;
+        priority_queue<long long> pq;
+        long long ans = 0;
+
+        for (vector<int>& r : roads) {
+            ++f[r[0]];
+            ++f[r[1]];
+        }
+
+        for (auto& e : f) {
+            pq.push(e.second);
+        }
+        while (!pq.empty()) {
+            long long curr = pq.top();
+            pq.pop();
+            ans += (curr * n);
+            --n;
+        }
+        return ans;
+    }
+};
